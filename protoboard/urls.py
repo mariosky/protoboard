15b725e5c12aee7994942db97f1f7d6f1b7c7794
Mario García Valdez <mariosky@gmail.com>
2021-04-27 17:42:35 -0700

    Registro

--- protoboard/urls.py
+++ protoboard/urls.py
@@ -66,15 +66,15 @@ urlpatterns = [
       path('accounts/', include('django_registration.backends.activation.urls')),
       path('accounts/', include('django.contrib.auth.urls')),
 
-    #  path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
-    #  path('accounts/logout/', auth_views.LogoutView.as_view()),
-    #  path('accounts/password_reset/', auth_views.PasswordResetView.as_view()),
+      path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
+      path('accounts/logout/', auth_views.LogoutView.as_view()),
+      path('accounts/password_reset/', auth_views.PasswordResetView.as_view()),
 
-   #   path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-   #   path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+      path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+      path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
-   #   path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-   #   path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-   #   path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-   #   path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+      path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+      path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+      path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+      path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
  ]
