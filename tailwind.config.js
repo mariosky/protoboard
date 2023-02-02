8472f3a5e5eba8915776e0ca45e75f4ddfd5d213
Mario Garcia-Valdez <mariosky@gmail.com>
2023-02-02 14:49:59 -0800

    un poco de espacio al header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
   content: ["./newtemplates/**/*.{html,js}", 
             "./newtemplates/activitytree/header.html"],
   theme: {
-    extend: {},
+    extend: {
+        typography: (theme) => ({
+                DEFAULT: {
+                  css: {
+                    color: theme('colors.gray.800'),
+                    pre: {
+                      color: theme("colors.grey.1000"),
+                      backgroundColor: theme("colors.grey.100")
+                    },
+                  },
+                },
+              }),
+    },
   },
-  plugins: [],
+
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
