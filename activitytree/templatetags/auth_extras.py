cb00166fdd291ce33d5f9d60321a0d3fde973eaa
Mario Garcia-Valdez <mariosky@gmail.com>
2023-03-10 14:06:25 -0800

    registration

--- activitytree/templatetags/auth_extras.py
+++ activitytree/templatetags/auth_extras.py
@@ -4,11 +4,9 @@ from django.contrib.auth.models import Group
 
 register = template.Library()
 
-
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    group = Group.objects.get(name=group_name)
-    return True if group in user.groups.all() else False
+    return user.groups.filter(name=group_name).exists()
 
 
 @register.filter
