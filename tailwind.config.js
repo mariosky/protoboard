4bed337b1e80819f35cf45dbe7a39b33a5f8ba3a
Mario Garcia-Valdez <mariosky@gmail.com>
2023-05-23 10:23:54 -0700

    new code section"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultCss = {
+  pre: false,
+};
+
+const smCss = {
+  pre: false,
+};
+
+const lgCss = {
+  pre: false,
+  h1: {
+    fontSize: '2.0em',
+    fontWeight: '700',
+  },
+};
+
+const xlCss = {
+  pre: false,
+};
+
+const xl2Css = {
+  pre: false,
+};
+
+
 module.exports = {
   content: ["./templates/**/*.{html,js}", 
             "./templates/activitytree/header.html"],
@@ -9,17 +34,15 @@ module.exports = {
           css: {
             'blockquote p:first-of-type::before': { content: 'none' },
             'blockquote p:first-of-type::after': { content: 'none' },
-          },
+              } 
+                 },
                 DEFAULT: {
                   css: {
-                    color: theme('colors.gray.800'),
-                    pre: {
-                      color: theme("colors.grey.1000"),
-                      backgroundColor: theme("colors.grey.100")
-                    },
+                    pre: false,
+                    "code::before": {content: ''},
+                    "code::after": {content: ''}
                   },
                 },
-              },
     }),
   }
   }, 
