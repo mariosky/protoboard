1e0d373f513e4241722d5930fba7d9dfef3c9e44
mariosky <mariosky@gmail.com>
2023-06-20 20:40:08 +0000

    changed the script for remote development

--- proto.sh
+++ proto.sh
@@ -1,3 +1,4 @@
+source venv/bin/activate
 if ! tmux has-session -t proto; then 
 	tmux new -s proto -d
 	tmux new-window -t proto:1 
@@ -6,8 +7,8 @@ if ! tmux has-session -t proto; then
 	tmux send-keys -t proto:1.0 "docker compose up" Enter
 	tmux split-window -t proto:1.0 -v 
 	tmux send-keys -t proto:1.1 "tailwindcss -i input.css -o ./activitytree/static/assets/css/output.css --watch" Enter
-	tmux send-keys -t proto:1.2 "python3 manage.py runserver" Enter
-	tmux send-keys -t proto:0 nvim Enter
+	tmux send-keys -t proto:1.2 "python3 manage.py runserver 0.0.0.0:8000" Enter
+	tmux send-keys -t proto:0 "nvim ." Enter
 
 fi
 	tmux attach -t proto
